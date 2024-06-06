84a4cb869177949d4307ed051e1c34607411710c
unmani-shinde <mailto.unmani@gmail.com>
2024-06-06 10:44:06 +0530

    env fixes and env.sample added

--- Algoverify-Codes/Frontend/app/issuer-page/components/pinFiletoIFPS.tsx
+++ Algoverify-Codes/Frontend/app/issuer-page/components/pinFiletoIFPS.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { config } from 'dotenv';
 
 const JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI1MGRmZWZjMy1kMGM5LTRlNzktYTA1Yi1jYmM5ZGMyZjY1MDgiLCJlbWFpbCI6Im1haWx0by51bm1hbmlAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siaWQiOiJGUkExIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9LHsiaWQiOiJOWUMxIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6ImIxYWQ1NzA3MTY1YjgwNWY2ZGNmIiwic2NvcGVkS2V5U2VjcmV0IjoiNDZhZTI2NDc4ZjJlNTgzZWI2ZTU2NzUwMDI3MzFjYTA5NDhiNDY2MTk5MTJiODJlZDI5NTM5MTcyYzkzYTk5MSIsImlhdCI6MTcxNzU4ODcyM30.wWGZpn4rxr8U-iBBmClFqqAdiZi89sCVTbLRXxkm-Hw'
 
@@ -26,7 +27,7 @@ async function pinFiletoIPFS(file:Blob) {
             
           headers: {
             'Content-Type': `multipart/form-data`,
-            'Authorization': `Bearer ${JWT}`
+            'Authorization': `Bearer ${process.env.NEXT_PUBLIC_PINATA_API_JWT?.toString()}`
           }
 		  });
           const university_hash = res.data.IpfsHash;
